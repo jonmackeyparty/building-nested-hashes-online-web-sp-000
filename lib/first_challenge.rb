dfdb6757759feaab7b8450a4622e18c158235df4
Jon Mackey <jon@jonmackey.party>
2018-11-27 19:28:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,32 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     montague: {
+       patriarch: {
+         attributes: {
+           name: "Lord Montague",
+           age: "53"}
+       }
+       matriarch: {
+         attributes: {
+           name: "Lady Monague",
+           age: "54"}
+         }
+       hero: {
+         attributes: {
+           name: "Romeo", 
+           status: "Alive"}
+         }
+       hero/s friends: {
+         friend 1: {
+          name: "Benviolo",
+          age: "30",
+          attitude: "confused"}
+        friend 2: {
+          name: "Nurse", 
+          age: "44",
+          attitude: "worried"}
+        }
+         }
+     capulet: {}
+   }
 end
