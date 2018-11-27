9bc2abdf04795c84b75151a51cdd078384b0d723
Jon Mackey <jon@jonmackey.party>
2018-11-27 19:48:00 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,25 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {
-       :patriarch => {
-           :name => "Lord Montague",
-           :age => "53"},
-       :matriarch => {
-           :name => "Lady Monague",
-           :age => "54"},
-       :hero => {
-           :name => "Romeo", 
-           :status => "Alive"},
-       :hero_friends => [{
-           :name => "Benviolo",
-           :age => "30",
-           :attitude => "confused"
-           },
-           {:name => "Nurse", 
-           :age => "44",
-           :attitude => "worried"}]
-      },
-         
+     :montague => {},
      :capulet => {}
    }
 end
